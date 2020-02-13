209e635667b218cebb7f6b36852992a71d915740
bisbaldi <annika.baldi@googlemail.com>
2020-02-13 21:31:28 +0100

    Add first screw route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Extending the printmedia' });
 });
 
 module.exports = router;
