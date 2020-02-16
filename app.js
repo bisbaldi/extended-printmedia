17b0f555bbbeb7dff7e03aa224d1f9cd2f4e41f3
bisbaldi <annika.baldi@googlemail.com>
2020-02-16 16:49:41 +0100

    Add second screw route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var screw1Router = require('./routes/screw1')
+var screw2Router = require('./routes/screw2')
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/screw1', screw1Router);
+app.use('/screw2', screw2Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
